4bcd3db95fb32e4d7cc698aebb027e5c7a54cd9f
Yuki <79082912+yukirizky@users.noreply.github.com>
2021-02-23 23:46:18 +0700

    belajar python

---
+++ test.py
@@ -0,0 +1,16 @@
+# inisiasi variabel
+panjang = 0
+lebar = 0
+luasPersegiPanjang = 0
+
+# input
+panjang = 20
+lebar = 30
+
+# proses
+luasPersegiPanjang = panjang*lebar
+
+# hasil
+print("panjang : ", panjang)
+print("lebar : ", lebar)
+print("luas persegi panjang : ", luasPersegiPanjang)
